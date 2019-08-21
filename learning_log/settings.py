89e8b9acb3285fc8f63dde9a81d150f06cd93bea
kosTest <kosTest@mail.ru>
2019-08-21 21:14:02 +0300

    Ready to heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,7 +137,7 @@ BOOTSTRAP4 = {
 if os.getcwd() == '/app':
     import dj_database_url
     DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost')
+        'default': dj_database_url.config(default='postgres://localhost')
     }
     # Поддержка заголовка 'X-Forwarded-Proto' для request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -147,5 +147,5 @@ if os.getcwd() == '/app':
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'static'),
     )
